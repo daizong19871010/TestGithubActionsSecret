df8daba6d5843c49ca32cbd43a9281b0bf339f1e
shoguncao <shoguncao@tencent.com>
2025-04-19 12:47:01 +0800

    改用printf

--- TestGithubActionsSecret.sh
+++ TestGithubActionsSecret.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.ssh/config.d
 echo "Include config.d/*.conf" >> ~/.ssh/config
 echo ${SHOGUNCAO_HAVA_A_TEST_SSH_KEY} > ~/.ssh/config.d/hava_a_test
 chmod 600 ~/.ssh/config.d/hava_a_test
-echo "Host github.com-hava_a_test\n  HostName github.com\n  User git\n  IdentityFile ~/.ssh/config.d/hava_a_test" > ~/.ssh/config.d/hava_a_test.conf
+printf "Host github.com-hava_a_test\n  HostName github.com\n  User git\n  IdentityFile ~/.ssh/config.d/hava_a_test\n" > ~/.ssh/config.d/hava_a_test.conf
 
 
 echo "cat ~/.ssh/config"
