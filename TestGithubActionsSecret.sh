8547c39bbc4210231ac3f62acbedf3a43362061a
shoguncao <shoguncao@tencent.com>
2025-04-19 12:53:02 +0800

    改用HOME

--- TestGithubActionsSecret.sh
+++ TestGithubActionsSecret.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-mkdir -p ~/.ssh/config.d
-echo "Include config.d/*.conf" >> ~/.ssh/config
-echo ${SHOGUNCAO_HAVA_A_TEST_SSH_KEY} > ~/.ssh/config.d/hava_a_test
-chmod 600 ~/.ssh/config.d/hava_a_test
-printf "Host github.com-hava_a_test\n  HostName github.com\n  User git\n  IdentityFile ~/.ssh/config.d/hava_a_test\n" > ~/.ssh/config.d/hava_a_test.conf
+mkdir -p "$HOME/.ssh/config.d"
+echo "Include config.d/*.conf" >> $HOME/.ssh/config
+chmod 700 "$HOME/.ssh"
+chmod 700 "$HOME/.ssh/config.d"
 
+echo "${SHOGUNCAO_HAVA_A_TEST_SSH_KEY}" > "$HOME/.ssh/config.d/hava_a_test"
+chmod 600 "$HOME/.ssh/config.d/hava_a_test"
+printf "Host github.com-hava_a_test\n  HostName github.com\n  User git\n  IdentityFile ~/.ssh/config.d/hava_a_test\n" > "$HOME/.ssh/config.d/hava_a_test.conf"
 
 echo "cat ~/.ssh/config"
 cat ~/.ssh/config
