0cda97f1c8f0c631c77136ddde36f2c3f1e106ec
shoguncao <shoguncao@tencent.com>
2025-04-19 12:43:08 +0800

    改小写

--- TestGithubActionsSecret.sh
+++ TestGithubActionsSecret.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.ssh/config.d
 echo "Include config.d/*.conf" >> ~/.ssh/config
-echo ${SHOGUNCAO_HAVA_A_TEST_SSH_KEY} > ~/.ssh/config.d/HAVA_A_TEST
-chmod 600 ~/.ssh/config.d/HAVA_A_TEST
-echo "Host github.com-HAVA_A_TEST\n  HostName github.com\n  User git\n  IdentityFile ~/.ssh/config.d/HAVA_A_TEST" > ~/.ssh/config.d/HAVA_A_TEST.conf
-git clone git@github.com-HAVA_A_TEST:shoguncao/hava_a_test.sh
+echo ${SHOGUNCAO_HAVA_A_TEST_SSH_KEY} > ~/.ssh/config.d/hava_a_test
+chmod 600 ~/.ssh/config.d/hava_a_test
+echo "Host github.com-hava_a_test\n  HostName github.com\n  User git\n  IdentityFile ~/.ssh/config.d/hava_a_test" > ~/.ssh/config.d/hava_a_test.conf
+git clone git@github.com-hava_a_test:shoguncao/hava_a_test.sh
