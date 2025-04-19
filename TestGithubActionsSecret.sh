3af6b7b090fa34cf656806fab68681ec3665f8cb
shoguncao <shoguncao@tencent.com>
2025-04-19 12:55:44 +0800

    修改添加ssh的方式

--- TestGithubActionsSecret.sh
+++ TestGithubActionsSecret.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+# 创建ssh的相关配置
 mkdir -p "$HOME/.ssh/config.d"
-echo "Include config.d/*.conf" >> $HOME/.ssh/config
+if [ ! -f "$HOME/.ssh/config" ]; then
+    touch "$HOME/.ssh/config"
+fi
+if ! grep -q "Include config.d/\*.conf" "$HOME/.ssh/config"; then
+    {
+        printf "Include config.d/*.conf\n\n"
+        cat "$HOME/.ssh/config"
+    } > "$HOME/.ssh/config.tmp" && mv "$HOME/.ssh/config.tmp" "$HOME/.ssh/config"
+fi
 chmod 700 "$HOME/.ssh"
 chmod 700 "$HOME/.ssh/config.d"
 
+# 写入hava_a_test的密钥
+rm -rf "$HOME/.ssh/config.d/hava_a_test"
 echo "${SHOGUNCAO_HAVA_A_TEST_SSH_KEY}" > "$HOME/.ssh/config.d/hava_a_test"
 chmod 600 "$HOME/.ssh/config.d/hava_a_test"
-printf "Host github.com-hava_a_test\n  HostName github.com\n  User git\n  IdentityFile ~/.ssh/config.d/hava_a_test\n" > "$HOME/.ssh/config.d/hava_a_test.conf"
 
-echo "cat ~/.ssh/config"
-cat ~/.ssh/config
-echo "~/.ssh/config.d/hava_a_test"
-cat ~/.ssh/config.d/hava_a_test
-echo "~/.ssh/config.d/hava_a_test.conf"
-cat ~/.ssh/config.d/hava_a_test.conf
+# 创建$HOME/.ssh/config.d/hava_a_test.conf
+rm -rf "$HOME/.ssh/config.d/hava_a_test.conf"
+cat > "$HOME/.ssh/config.d/hava_a_test.conf" << EOF
+Host github.com-hava_a_test
+    HostName github.com
+    IdentityFile ~/.ssh/config.d/hava_a_test
+EOF
+
+echo "cat $HOME/.ssh/config"
+cat "$HOME/.ssh/config"
+echo "cat $HOME/.ssh/config.d/hava_a_test"
+cat "$HOME/.ssh/config.d/hava_a_test"
+echo "cat $HOME/.ssh/config.d/hava_a_test.conf"
+cat "$HOME/.ssh/config.d/hava_a_test.conf"
 
 git clone git@github.com-hava_a_test:shoguncao/hava_a_test.sh
